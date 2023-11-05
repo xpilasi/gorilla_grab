ef593f83544050c70d161a1f7f8addd9bf6fec00
xpilasi <pilasiha@gmail.com>
2023-11-05 21:42:03 +0100

    buttons updated

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gorilla_grab/Screens/my_trainings/session/training_session_provisional_screen.dart';
