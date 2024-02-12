c4a11ecbd3409cc746403db29bfe2be10f33d8ff
xpilasi <pilasiha@gmail.com>
2024-02-12 20:15:10 +0100

    profile controller Firebase test

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,8 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gorilla_grab/constants/texts.dart';
 
 class ProfileController extends GetxController {
+//Loading data from Firebase
+  late final db = FirebaseFirestore.instance;
+  late final usersMap = db.collection('users');
+  final userEmail = FirebaseAuth.instance.currentUser!.email;
+
+  late final userProfileData = FirebaseFirestore.instance
+      .collection('users')
+      .doc(userEmail)
+      .collection('profile');
+
+//Recovering profile data:
+
+  var profile = {
+    'name': 'John',
+    'lastName': 'Doe',
+    'city': 'noMansLand',
+    'country': 'noCountry',
+    'email': 'noEmail',
+    'climbinSince': 2000,
+    'nationality': 'lostPerson',
+  };
+  @override
+  void onInit() {
+    super.onInit();
+    _loadProfile();
+  }
+
+  void _loadProfile() async {
+    final userProfile = await userProfileData.get();
+    final profileDocument = userProfile.docs.first;
+
+    profile['name'] = profileDocument['name'];
+    profile['lastName'] = profileDocument['lastName'];
+    profile['city'] = profileDocument['city'];
+    profile['country'] = profileDocument['country'];
+    profile['email'] = profileDocument['email'];
+    profile['climbinSince'] = profileDocument['climbinSince'];
+    profile['nationality'] = profileDocument['nationality'];
+  }
+
 //To update the profile basic data
 
   void updateProfileBasicData(
